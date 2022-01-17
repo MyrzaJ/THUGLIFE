fb742cb4ac03e9b4ae5b7d50440ea8d0af0d6b19
Myrza8 <myrza080803@gmail.com>
2022-01-17 22:09:04 +0600

    first

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+
+import First from '../first/First'
+
+import "./app.scss"
+
+function App() {
+  return (
+    <div className="appContainer">
+     <First/>
+    </div>
+ 
+  );
+}
+
+export default App;
